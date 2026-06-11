5939e9fa0383a43f69449e18a253a3936637a229
Nuriia9075 <a.nuriia.m02@gmail.com>
2026-06-11 18:07:30 +0600

    updated forms.py

--- source/todo/forms.py
+++ source/todo/forms.py
@@ -4,18 +4,18 @@ from todo.models.status_type import Status,Type
 
 
 class AddTodoForm(forms.ModelForm):
+    status = forms.ModelChoiceField(
+        queryset=Status.objects.all(),
+        label="Статус",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    type = forms.ModelMultipleChoiceField(
+        queryset=Type.objects.all(),
+        label="Тип",
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'})
+    )
     class Meta:
         model = Task
-        status = forms.ModelChoiceField(
-            queryset=Status.objects.all(),
-            label="Статус",
-            widget=forms.Select(attrs={'class': 'form-control'})
-        )
-        type = forms.ModelMultipleChoiceField(
-            queryset=Type.objects.all(),
-            label="Тип",
-            widget=forms.SelectMultiple(attrs={'class': 'form-control'})
-        )
         fields =[
             'title',
             'description',
